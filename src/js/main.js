25779e5fc6dc9f065bf8f9327878f8605b719acd
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-06-05 11:50:39 +0300

    feat: добавляет код слайдера изображений WIP

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-const siteHeader = document.querySelector(`.site-header`);
+const activateScrollSiteHeader = () => {
+  const siteHeader = document.querySelector(`.site-header`);
 
-const scrollSiteHeaderHandler = () => {
   let lastScrollPosition = 0;
 
   window.addEventListener(`scroll`, () => {
@@ -18,8 +18,6 @@ const scrollSiteHeaderHandler = () => {
   });
 };
 
-scrollSiteHeaderHandler();
-
 const smoothScrollToSection = (sectionID, buttonID) => {
   const section = document.querySelector(sectionID);
   const button = document.querySelector(buttonID);
@@ -31,5 +29,23 @@ const smoothScrollToSection = (sectionID, buttonID) => {
   });
 };
 
+const activateImageMobileSlider = () => {
+  const mainInfoSlider = document.querySelector(`.main-info__slider`);
+  const mainInfoSlides = mainInfoSlider.querySelectorAll(`.slider__slide`);
+  let currentActiveSlide = 0;
+
+  mainInfoSlider.addEventListener(`click`, () => {
+    if ((currentActiveSlide + 1) !== mainInfoSlides.length) {
+      mainInfoSlides[currentActiveSlide].classList.remove(`slider__slide--active`);
+      currentActiveSlide++;
+      mainInfoSlides[currentActiveSlide].classList.add(`slider__slide--active`);
+    } else {
+      console.log(`end`)
+    }
+  });
+}
+
+activateScrollSiteHeader();
 smoothScrollToSection(`#main-info`, `#main-info-scroll-link`);
 smoothScrollToSection(`#news-section`, `#news-scroll-link`);
+activateImageMobileSlider();
